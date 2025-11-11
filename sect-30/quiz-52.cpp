338c542bad10992679437be1ad9de4e65af9b84b
Cn <TJKG9Z3@vw.vwgroup.com>
2025-11-11 19:25:10 +0530

    Friends - added quiz-52, which was missed earlier.

---
+++ sect-30/quiz-52.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+ 
+class Point{
+    //friend double distance(const Point& from, const Point& to);
+    friend class ShapeFactory;
+    public : 
+    //methods
+    Point() = delete;
+    Point(double x, double y)
+        : m_x(x), m_y(y){
+    }
+ 
+    Point(const Point& p)
+     : m_x(p.x()), m_y(p.y())
+    {
+        std::cout << "cpy ctor called..." << std::endl;
+    }
+ 
+ 
+ 
+    void set_x(double x){
+        m_x = x;
+    }
+ 
+    void set_y(double y){
+        m_y = y;
+    }
+ 
+    double x() const   {
+        return m_x;
+    }
+    double y() const   {
+        return m_y;
+    }
+    //member variables
+    private : 
+    static const size_t s_point_count{};
+    double m_x{1};
+    double m_y{1};
+};
+ 
+void print_point(Point& p){
+    std::cout << "Point[x : " << p.x() << ", y : " << p.y() << "]" ;
+}
+ 
+int main()
+{
+    std::cout << "Point count : " << Point::s_point_count << std::endl;
+    return 0;
+}
