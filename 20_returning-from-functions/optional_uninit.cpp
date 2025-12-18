72deb6ba292f8a9d04e2b6e087c4cf5539bfec4a
Aananth C N <c.n.aananth@gmail.com>
2025-12-18 10:41:03 +0530

    Clarify exception for uninitialized std::optional
    Added comment explaining exception for uninitialized optional.

--- 20_returning-from-functions/optional_uninit.cpp
+++ 20_returning-from-functions/optional_uninit.cpp
@@ -4,6 +4,10 @@
 int main(){
  
     std::optional<int> age{};
+
+    // The following line will throw `bad optional access` exception as
+    // age is not initialized with a proper value.
     std::cout << "value : " << age.value() << std::endl;
+
     return 0;
 }
